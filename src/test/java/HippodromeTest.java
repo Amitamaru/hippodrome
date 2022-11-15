45b22da1fa48c61d8b4ef95b0472229b01f793f0
Dmytro M <dmytromarzhiievskyi@gmail.com>
2022-11-15 13:28:52 +0200

    tested hippodrome.Move and hippodrome.getHorses

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,10 +1,15 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
 
+@ExtendWith(MockitoExtension.class)
 class HippodromeTest {
     @Test
     void checkNullParameter() {
@@ -34,6 +39,32 @@ class HippodromeTest {
         } catch (IllegalArgumentException e) {
             assertEquals("Horses cannot be empty.", e.getMessage());
         }
+    }
+
+    @Test
+    void gerHorsesTest() {
+        List<Horse> horsesList = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            horsesList.add(new Horse("plotva" + i, 1));
+        }
+        Hippodrome hippodrome = new Hippodrome(horsesList);
+        assertEquals(horsesList, hippodrome.getHorses());
+    }
+
+
+
+    @Test
+    void moveTest() {
+        Horse plotva = Mockito.mock(Horse.class);
+        Mockito.doNothing().when(plotva).move();
+        List<Horse> horsesList = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            horsesList.add(plotva);
+        }
+        Hippodrome hippodrome = new Hippodrome(horsesList);
+        hippodrome.move();
+
+        Mockito.verify(plotva, times(50)).move();
     }
 
 }
